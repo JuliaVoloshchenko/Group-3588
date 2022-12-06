94379f348c37e84d2d0850b91eec1885f27bf2f8
Julia <juliaf92@mail.ru>
2022-12-06 12:04:47 +0300

    fixed a bug in the task

--- Sem2HomeTask13/Program.cs
+++ Sem2HomeTask13/Program.cs
@@ -1,19 +1,15 @@
 ﻿//Вводим число
 int number = int.Parse(Console.ReadLine() ?? "0");
-
-
 //Вводим условие
 if (number > 99)
 {
-    //Превращаем число в массив
-    Char[]digits = number.toString().toChar(Arrey).;
-
-    //Выводим в консоле результат
-    Console.WriteLine(digits[2]);
+//Превращаем число в массив
+Char[] digits = number.ToString().ToCharArray();
+//Выводим в консоле результат
+Console.WriteLine(digits[2]);
 }
 else
 {
-    //Выводим в консоле, что в числе нет третьей цифры
-    Console.WriteLine("в числе нет третьей цифры");
-
+//Выводим в консоле, что в числе нет третьей цифры
+Console.WriteLine("в числе нет третьей цифры");
 }
