568bf13dc7effec505111157126b882182dcb715
Julia <juliaf92@mail.ru>
2022-12-08 18:09:15 +0300

    Added changes

--- Sem3HomeTask19/Program.cs
+++ Sem3HomeTask19/Program.cs
@@ -3,19 +3,19 @@ int num = ReadData("Введите пятизначное число: ");
 bool result = PalinTest(num);
 PrintData("Является ли число палиндромом?", result);
 
-//Метод сравнения
-bool PalinTest(int num)
-{
-    bool result = false;
-    result = (num / 10000 == num % 10) && ((num / 1000) % 10 == (num / 10) % 10);
-    return result;
-}
-
 //Метод читает данные от пользователя
 int ReadData(string msg)
 {
     Console.Write(msg);
     return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод проверки, является ли число палиндромом
+bool PalinTest(int num)
+{
+    bool result = false;
+    result = (num / 10000 == num % 10) && ((num / 1000) % 10 == (num / 10) % 10);
+    return result;
 }
 
 // Метод вывода данных
