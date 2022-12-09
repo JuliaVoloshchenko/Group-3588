2fa6ae3e0ac82432ee17d02cdc36be6b2d87a60e
Julia <juliaf92@mail.ru>
2022-12-09 22:11:07 +0300

    added solution for task 24, 26, 28, 30

---
+++ Sem4Task26/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// Console.Write("Введите число: ");
+
+// string? NumberStr = Console.ReadLine();
+// int Number = int.Parse(NumberStr??"0");
+
+// Console.WriteLine((int)(1+Math.Log10(Number)));
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+int VariantLog(int num)
+{
+    int count = (int)Math.Log10(num)+1;
+    return count;
+}
+int number = ReadData("Введите число А:");
+int length = VariantLog(number);
+PrintData("Количество цифр в числе равно^ " + length);
