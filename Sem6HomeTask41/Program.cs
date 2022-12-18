d865f20d4201cd79e247d8b78a4f7736b8f2ae9e
Julia <juliaf92@mail.ru>
2022-12-18 21:34:32 +0300

    Added a solution for hometasks 41,43

---
+++ Sem6HomeTask41/Program.cs
@@ -0,0 +1,28 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int N = ReadData("Введите колличество чисел: ");
+int result = CountPosNum(N);
+PrintData("Колличество чисел больше 0 = ", result);
+
+//Метод, считывающий данные, введенные пользователем
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+//Метод, выводящий на печать данные пользователю
+void PrintData(string msg, int res)
+{
+    Console.WriteLine(msg + res);
+}
+//Метод подсчитывающий колличество положительных чисел
+int CountPosNum(int N)
+{
+    int res = 0;
+    while (N > 0)
+    {
+        if (ReadData("Введите число: ") > 0)
+            res++;
+        N = N - 1;
+    }
+    return res;
+}
---
+++ Sem6HomeTask43/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+double k1 = ReadData("Введите значение K1: ");
+double b1 = ReadData("Введите значение B1: ");
+double k2 = ReadData("Введите значение K2: ");
+double b2 = ReadData("Введите значение B2: ");
+double x = (-b2 + b1) / (-k1 + k2);
+double y = k2 * x + b2;
+PrintData($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+
+//Метод, считывающий данные, введенные пользователем
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+//Метод, выводящий на печать данные пользователю
+void PrintData(string msg)
+{
+    Console.WriteLine(msg);
+}
