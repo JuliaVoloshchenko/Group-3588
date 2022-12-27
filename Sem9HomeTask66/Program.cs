1b767cbfe16560c98e77bd0218b95cab52c40087
Julia <juliaf92@mail.ru>
2022-12-27 20:05:43 +0300

    Added a solutions for hometask 64,66,68

---
+++ Sem9HomeTask66/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Печать результата
+void PrintResult(int prefix)
+{
+    Console.WriteLine(prefix);
+}
+
+//Рекурентный метод нахождения суммы чисел строки
+int SumMNRec(int M, int N)
+{
+    if (M >= N)
+    {
+        return N;
+    }
+    else
+    {
+        return M + SumMNRec(M + 1, N);
+    }
+}
+
+
+int M = ReadData("Введите число M: ");
+int N = ReadData("Введите число N: ");
+int Sum = SumMNRec(M, N);
+PrintResult(Sum);
---
+++ Sem9HomeTask68/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Печать результата
+void PrintResult(int prefix)
+{
+    Console.WriteLine(prefix);
+}
+
+//Рекурсивный метод вычисления функции Аккермана для малых значений
+int AkkermanFunc(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return AkkermanFunc(n - 1, 1);
+    else
+        return AkkermanFunc(n - 1, AkkermanFunc(n, m - 1));
+}
+
+int m = ReadData("Введите число m, которое должно быть <= 3: ");
+int n = ReadData("Введите число n, которое должно быть <= 11: ");
+int function = AkkermanFunc(m, n);
+PrintResult(function);
